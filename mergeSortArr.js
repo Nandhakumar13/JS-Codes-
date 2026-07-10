4a96d07b7ebc8741dc5517cc37ed085c8c3acfbb
Nandhakumar <nandhakumarm1999@gmail.com>
2026-07-10 08:52:45 +0530

    Merge sort array method reimplementation

--- mergeSortArr.js
+++ mergeSortArr.js
@@ -1,11 +1,9 @@
-// merge sorting of given array
-
 function mergeSort(arr){
     if(arr.length <= 1) return arr;
 
-    let med = Math.floor(arr.length/2);
-    let left  = mergeSort(arr.slice(0,med));
-    let right  = mergeSort(arr.slice(med));
+    let med = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0,med));
+    let right = mergeSort(arr.slice(med));
 
     return mergeArr(left,right);
 }
@@ -16,18 +14,18 @@ function mergeArr(left,right){
     let resArr = [];
 
     while(i < left.length && j < right.length){
-        if(left[i] < right[j]){
+        if(left[i] < right[j] ){
             resArr.push(left[i]);
             i++;
         }
         else{
             resArr.push(right[j]);
-            j++;
+            j++
         }
     }
-
     return resArr.concat(left.slice(i).concat(right.slice(j)));
+    
 }
 
 
-console.log("Sorted Array :: Merge sort", mergeSort([2,4,1,5,2,9,7,5]));
+console.log("Merge sort :: Array sorting method", mergeSort([3,6,2,1,8,0,4]));
