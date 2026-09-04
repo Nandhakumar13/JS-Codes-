04b391c21e14914cf7694db2d14a97a375055e6c
Nandhakumar <nandhakumarm1999@gmail.com>
2026-09-04 15:17:14 +0530

    Some changes

--- array/findDuplicates.js
+++ array/findDuplicates.js
@@ -37,3 +37,30 @@ function setMethod(arr){
 // console.log("find duplicate", findDuplicate([1,2,3,4,2]));
 // console.log("find duplicate", indexOf([1,2,3,4,2]));
 console.log("find duplicate", setMethod([1,2,3,4]));
+
+
+function longNonRepetativeStr(str){
+    let set = new Set();
+    let left = 0;
+    let right = 0;
+    let maxLength = 0;
+    let result = 0;
+
+    while(left < str.length - 1){
+        if(!set.has(str[right])){
+            maxLength+=1;
+            result = Math.max(maxLength, result);
+            right++;
+        }
+        else{
+            result = 0;
+            left++;
+            right = left;
+
+        }
+        set.add(str[right]);
+    }
+    return set;
+}
+
+console.log("Longest non repetative string", longNonRepetativeStr("au"));
--- longestNonRepetativeString.js
+++ longestNonRepetativeString.js
@@ -2,13 +2,20 @@ function longestNonRepeativeStr(str) {
   let left = 0;
   let maxLength = 0;
   let set = new Set();
-  for (let right = 0; i < str.length; i++) {
-    while (set.has(right)) {
-      set.delete(left);
+  let n = str.length;
+
+  for(let i = 0; i < n;i++){
+    while(set.has(str[i])){
+      set.delete(str[left]);
       left++;
     }
-    set.add(right);
-    maxLength = Math.max(maxLength,right - left +1);
+    set.add(str[i]);
+    maxLength = Math.max(i - left+1,maxLength);
   }
+  console.log(set);
   return maxLength;
+
 }
+
+
+console.log("Longest non repetative str is", longestNonRepeativeStr("pwwkew"));
