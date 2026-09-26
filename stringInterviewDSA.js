636535477c9f601ef3d764eff7abf47a7d53c24c
Nandhakumar <nandhakumarm1999@gmail.com>
2026-09-26 11:17:09 +0530

    String Interview Preparation :: 03 Valid Anagram

--- stringInterviewDSA.js
+++ stringInterviewDSA.js
@@ -44,7 +44,33 @@ function validPalindrome(str){
     return true;
 
 }
-let s1 = "A man, a plan, a canal: Panama";
-let s2 = "race a car";
-console.log("check valid palindrom", validPalindrome(s1));
-console.log("check valid palindrom", validPalindrome(s2));
+// let s1 = "A man, a plan, a canal: Panama";
+// let s2 = "race a car";
+// console.log("check valid palindrom", validPalindrome(s1));
+// console.log("check valid palindrom", validPalindrome(s2));
+
+// Problem #3 — Valid Anagram
+
+function validAnagram(s1,s2){
+    if(s1.length !== s2.length) return false;
+
+    let  charArr = new Array(26).fill(0);
+
+    for(const char of s1){
+        charArr[char.charCodeAt(0) - 97]++;
+    }
+
+    for(const char of s2){
+        let idx = char.charCodeAt(0) - 97;
+
+        if(charArr[idx] == 0) return false;
+        charArr[idx]--;
+    }
+
+    return true;
+}
+// s = "rat"
+// t = "car"
+// console.log("Valid anagram", validAnagram(s,t));
+
+
